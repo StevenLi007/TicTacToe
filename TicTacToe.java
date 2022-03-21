0e21ec61a53258eb6bfb34b72b0d6f9659dbf580
StevenLi007 <sil045@ucsd.edu>
2022-03-21 16:45:16 -0700

    Set up framework of the game
    Added:
    - imports
    - method headers
    - some documentation
    - pseudocode

---
+++ TicTacToe.java
@@ -0,0 +1,70 @@
+/**
+ * TODO: file header
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * TODO: class header
+ */
+public class TicTacToe {
+    ArrayList<String[]> board;
+
+    /**
+     * Initializes an empty 3x3 board.
+     */
+    public TicTacToe() {
+        // TODO: implementation
+    }
+
+    /**
+     * Plays the game using the user 
+     * inputted moves.
+     * TODO: it relies on many helper methods; how do we make that clear (curr description a bit vague)?
+     */
+    public void play() {
+        // TODO: implementation
+        // print current board
+        // ask for player move
+        // make player move
+        // check for win
+        // ask for computer move
+        // make computer move
+        // check for win
+        // print current board
+        // depending on if game ends, repeat or end game
+    }
+
+    // get the user input
+    // what format?
+    // this may be unnecessary
+    public String getPlayerMove() {
+        // TODO: implementation
+        return "";
+    }
+
+    // generate computer move
+    // idea for future: multiple difficulties
+    public String getComputerMove() {
+        // TODO: implementation
+        return "";
+    }
+
+    // has anyone won?
+    // is there an efficient way to check?
+    public String getWinner() {
+        // TODO: implementation
+        return "";
+    }
+
+    /**
+     * Returns a string representation of the
+     * game board.
+     * @return string representation of the
+     * game board.
+     */
+    public String toString() {
+        // TODO: implementation
+        return "";
+    }
+}
