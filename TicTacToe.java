e641beea1e729b17af5a2e7bc2f10aef60c539f2
StevenLi007 <sil045@ucsd.edu>
2022-03-21 17:14:45 -0700

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,37 +1,43 @@
 /**
  * TODO: file header
  */
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * TODO: class header
  */
 public class TicTacToe {
-    ArrayList<String[]> board;
+    String[][] board;
 
     /**
      * Initializes an empty 3x3 board.
      */
     public TicTacToe() {
-        // TODO: implementation
+        this.board = new String[3][3];
     }
 
     /**
-     * Plays the game using the user 
-     * inputted moves.
+     * Plays the game.
      * TODO: it relies on many helper methods; how do we make that clear (curr description a bit vague)?
      */
     public void play() {
         // TODO: implementation
         // print current board
+        System.out.println(this.toString());
         // ask for player move
+        Scanner input = new Scanner(System.in);
+        String playerMove = input.nextLine();
+        // parse player move (how to check for inappropriate input?)
+        int row = Integer.parseInt(playerMove.substring(0, 1));
+        int col = Integer.parseInt(s)
+        // check player move validity
         // make player move
         // check for win
         // ask for computer move
         // make computer move
         // check for win
         // print current board
+        System.out.println(this.toString());
         // depending on if game ends, repeat or end game
     }
 
@@ -66,5 +72,10 @@ public class TicTacToe {
     public String toString() {
         // TODO: implementation
         return "";
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
     }
 }
